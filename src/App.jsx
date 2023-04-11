7410289ee5b47f366eefe20caabaf0dfc10089f2
Ahmed Ali M. Soliman <40831708+ahmedamsoliman-1@users.noreply.github.com>
2023-04-11 21:10:15 +0000

    add docs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import ListCast from './components/ListCast';
-import Modals from './components/Modals';
-import Nav from './components/Nav';
+import React, { useEffect, useState } from "react";
+import ListCast from "./components/ListCast";
+import Modals from "./components/Modals";
+import Nav from "./components/Nav";
 import "./App.scss";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   let [memberInfo, setMemberInfo] = useState(null);
 
   async function fetchCast() {
-    const response = await fetch('cast.json');
+    const response = await fetch("cast.json");
     setCast(await response.json());
   }
 
@@ -19,17 +19,42 @@ function App() {
 
   return (
     <>
-      <Nav cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
+      <Nav
+        cast={cast}
+        onChoice={(info) => {
+          setMemberInfo(info);
+        }}
+      />
       <div className="container">
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the Stargazers</h1>
-          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <ListCast cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
-          {memberInfo && <Modals member={memberInfo} handleChange={(info) => { setMemberInfo(cast[info]) }} handleClose={() => { setMemberInfo(null) }} />}
+          <h1>Meet the Stargazers Edit</h1>
+          <p>
+            Members of an <b>intergalactic alliance</b> paving the way for peace
+            and benevolence among all species. They are known for their
+            enthusiasm for science, for their love of fun, and their dedication
+            to education.
+          </p>
+          <ListCast
+            cast={cast}
+            onChoice={(info) => {
+              setMemberInfo(info);
+            }}
+          />
+          {memberInfo && (
+            <Modals
+              member={memberInfo}
+              handleChange={(info) => {
+                setMemberInfo(cast[info]);
+              }}
+              handleClose={() => {
+                setMemberInfo(null);
+              }}
+            />
+          )}
         </hgroup>
       </div>
     </>
-  )
+  );
 }
-export default App
+export default App;
